4b46d2bbaf135b1577d01113f67dfd87bd1fde30
olymoodbughnes <yairomanuel@gmail.com>
2022-09-25 14:52:20 +0100

    upload

---
+++ Assets/Prefabs/TopRooms/MidFinish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MidFinish : MonoBehaviour
+{
+    [SerializeField]
+    ScoreKeeper sKeeper;
+
+    private void Start()
+    {
+        sKeeper = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreKeeper>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+
+            sKeeper.Tally();
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+            SceneManager.LoadScene("game");
+        }
+        }
+}
